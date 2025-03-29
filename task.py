943e21e7848e561a64d8497c7416411c2a62f12b
Abhishek242709 <adiyalaabhishekreddy09@gmail.com>
2025-03-29 07:27:15 +0530

    python task is commited

---
+++ task.py
@@ -0,0 +1,26 @@
+#Basic python script by using data types,arithimetic operation,variables declaration
+
+#Declare and print variables of different data types (string, integer, float, and boolean)
+
+string_var = "Abhishek,reddy!"  #String
+print("String variables:",string_var)
+int_var= 100   #integer
+print("integer variable:",int_var)
+float_var= 8.15 #float
+print("float variable:",float_var)
+boolean_var=True #boolean
+print("Boolean variables:",boolean_var)
+
+#Perform a basic arithmetic operation using numbers
+sum_result= int_var + float_var
+print("result of the arithmetic operation is:",sum_result)
+
+ #Convert an integer to a string and display the result.
+num = 123  # Integer
+num_str = str(num)  # Convert to string
+print("Strng value is:", num_str)  # Display the result
+
+
+
+
+
